33fa0f2fe253435f07ec138aedd8272b30e57b89
Ken_Inaba <rightman20020920@yahoo.co.jp>
2023-05-29 00:03:59 +0900

    test

---
+++ test/test.bash
@@ -0,0 +1,10 @@
+#!/bin/bash 
+#SPDX-FileCopyrightText:2022 Ken Inaba rightman20020920@yahoo.co,jp
+#SPDX-Licence-Identifire:BSD-3-Clause
+cd ../
+
+gcc Ulamsspiral.c -o Ulamsspiral -lm
+
+./Ulamsspiral
+
+
